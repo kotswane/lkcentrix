701bceaf52de6762da9f2e11c7ea7445e44b4ee5
Moeketsi Kotswane <moeketsi.kotswane@net1.com>
2023-06-13 18:19:53 +0200

    autocomplete

--- application/views/site.php
+++ application/views/site.php
@@ -6,7 +6,7 @@
     <title>LKCENTRIX SOLUTIONS - SEARCH</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.css" />
+	
     <link rel="stylesheet" href="<?php echo base_url();?>bower_components/bootstrap/dist/css/bootstrap.min.css">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="<?php echo base_url();?>bower_components/font-awesome/css/font-awesome.min.css">
